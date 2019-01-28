6298bb440350d2a966bf513308a20781b5e6850d
Jan Bölsche <regular.gonzales@googlemail.com>
2019-01-28 18:02:37 +0100

    Use ctx.contentObs for editor shell support

--- demo.js
+++ demo.js
@@ -4,59 +4,138 @@ const h = require('mutant/html-element')
 const Value = require('mutant/value')
 const MutantArray = require('mutant/array')
 const MutantMap = require('mutant/map')
+const computed = require('mutant/computed')
 const setStyle = require('module-styles')('tre-fonts-demo')
 const collectMutations = require('collect-mutations')
 const pull = require('pull-stream')
+const Finder = require('tre-finder')
+const Shell = require('tre-editor-shell')
+const Importer = require('tre-file-importer')
+const WatchMerged = require('tre-prototypes')
+const {makePane, makeDivider, makeSplitPane} = require('tre-split-pane')
 
-setStyle(`
-  body {
-    --tre-selection-color: green;
-    --tre-secondary-selection-color: yellow;
-  }
-  .tre-fonts-editor {
-    max-width: 500px;
-  }
-`)
+styles()
 
 client( (err, ssb, config) => {
   if (err) return console.error(err)
 
+  const watchMerged = WatchMerged(ssb)
+  const primarySelection = Value()
+  const merged_kv = computed(primarySelection, kv => {
+    const c = content(kv)
+    if (!c) return
+    return watchMerged(c.revisionRoot || kv.key)
+  })
+
+  console.log('config', config)
+  const importer = Importer(ssb, config)
+  importer.use(require('./common'))
+  
+  const renderFinder = Finder(ssb, {
+    importer,
+    primarySelection,
+    skipFirstLevel: true,
+    details: (kv, ctx) => {
+      return kv && kv.meta && kv.meta["prototype-chain"] ? h('i', '(has proto)') : []
+    }
+  })
+
+  const renderShell = Shell(ssb, {
+    save: (kv, cb) => {
+      ssb.publish(kv.value.content, cb)
+    }
+  })
+
   const renderFont = Fonts(ssb, {
     prototypes: config.tre.prototypes,
-    save: content => {
-      console.log('new content', content)
-      //ssb.publish(content)
-    }
   })
 
-  const kv = {
-    key: 'fake-key',
-    value: {
-      content: {
-        type: 'font',
-        files: []
-      }
-    }
-  }
+  const where = Value('editor')
+  let current_kv
+  let current_where
 
-  document.body.appendChild(
-    renderFont(kv, {
-      where: 'editor'
-    })
-  )
+  document.body.appendChild(h('.tre-stylesheets-demo', [
+    makeSplitPane({horiz: true}, [
+      makePane('25%', [
+        renderFinder(config.tre.branches.fonts || config.tre.branches.root)
+      ]),
+      makeDivider(),
+      makePane('70%', [
+        h('.bar', [
+          h('select', {
+            'ev-change': e => {
+              where.set(e.target.value)
+            }
+          }, [
+            h('option', 'editor'),
+            h('option', 'stage'),
+            h('option', 'thumbnail')
+          ])
+        ]),
+        computed([where, merged_kv], (where, kv) => {
+          if (
+            revisionRoot(kv) == revisionRoot(current_kv) &&
+            where == current_where
+          ) return computed.NO_CHANGE
+          current_kv = kv
+          current_where = where
+          if (!kv) return []
 
-  const fonts = MutantArray()
-  const fontElements = MutantMap(fonts, kv=>{
-    console.log('rendering', kv())
-    return renderFont(kv())
-  })
-  document.head.appendChild(
-    h('div.fonts', {}, fontElements)
-  )
-
-  pull(
-    ssb.revisions.messagesByType('font', {live: true, sync: true}),
-    collectMutations(fonts, {sync: true})
-  )
-
+          if (where !== 'editor') {
+            return renderFont(kv, {where})  
+          }
+          const contentObs = Value(Object.assign({}, kv.value.content))
+          return renderShell(kv, {
+            renderEditor: renderFont,
+            contentObs,
+            where
+          })
+        })
+      ])
+    ])
+  ]))
 })
+
+function content(kv) {
+  return kv && kv.value && kv.value.content
+}
+
+function revisionRoot(kv) {
+  return kv && kv.value.content && kv.value.content.revisionRoot || kv && kv.key
+}
+
+function styles() {
+  setStyle(`
+    body, html, .tre-stylesheets-demo {
+      height: 100%;
+      margin: 0;
+      padding: 0;
+    }
+    body {
+      --tre-selection-color: green;
+      --tre-secondary-selection-color: yellow;
+      font-family: sans-serif;
+    }
+    h1 {
+      font-size: 18px;
+    }
+    .pane {
+      background: #eee;
+    }
+    .tre-finder .summary select {
+      font-size: 9pt;
+      background: transparent;
+      border: none;
+      width: 50px;
+    }
+    .tre-finder summary {
+      white-space: nowrap;
+    }
+    .tre-finder summary:focus {
+      outline: 1px solid rgba(255,255,255,0.1);
+    }
+    .tre-fonts-editor {
+      max-width: 500px;
+    }
+  `)
+}
--- index.js
+++ index.js
@@ -11,105 +11,119 @@ const dropzone = require('tre-dropzone')
 const FileSource = require('tre-file-importer/file-source')
 const {importFiles} = require('./common')
 
-setStyle(`
-  .tre-fonts-editor .tre-dropzone {
-    min-width: 400px;
-    min-height: 50px;
-    border-radius: 20px;
-    border: 8px dashed #eee;
-    padding: 1em;
-    margin: 1em;
-  }
-  .tre-fonts-editor .tre-dropzone.drag {
-    border-color: #777;
-  }
-  .tre-fonts-editor .list {
-    display: grid; 
-    grid-template-columns: 4em 3em 1fr 5em 4em; 
-    width: min-content;
-  }
-  .tre-fonts-editor .list > * {
-    margin: .1em .2em;
-  }
-  .tre-fonts-editor .placeholder {
-    color: #555;
-  }
-`)
+module.exports = function(ssb, opts) {
+  opts = opts || {}
+  const {prototypes} = opts
+  if (!prototypes) throw new Error('need prototypes!')
 
-function RenderCSS(ssb) {
+  styles()
   const blobPrefix = Value()
   ssb.ws.getAddress((err, address) => {
     if (err) return console.error(err)
     address = address.replace(/^ws:\/\//, 'http://').replace(/~.*$/, '/blobs/get/')
     blobPrefix.set(address)
   })
-  
-  return function renderCSS(kv, ctx) {
-    const content = kv.value && kv.value.content
-    if (!content) return
 
-    const css = computed(blobPrefix, bp => {
-      const sources = (content.files || []).map( f => {
-        return `src: url("${bp + encodeURIComponent(f.link)}");`
-      }).join('\n')
+  return function render(kv, ctx) {
+    ctx = ctx || {}
+    const content = kv.value && kv.value.content
+    if (content.type !== 'font') return
+
+    function renderTile(kv, ctx) {
+      const content = kv.value && kv.value.content
+      if (!content) return
+      return h('div', {
+        style: { 
+          'font-family': content['font-family'],
+          'font-size': '45px'
+        }
+      }, [
+        renderStyleTag(kv),
+        'Aa'
+      ])
+    }
+
+    if (ctx.where == 'editor') {
+      return renderEditor(kv, ctx)
+    } else if (ctx.where == 'thumbnail') {
+      return renderTile(kv, ctx)
+    }
+    return renderStyleTag(kv, ctx)
+  }
+
+
+  function sourceFromFile(file) {
+    if (file.link) {
+      return computed(blobPrefix, bp => {
+        return `src: url("${bp + encodeURIComponent(file.link)}");`
+      })
+    } else {
+      const src = Value('')
+      dataUri(file, (err, uri) => {
+        if (err) return console.error(err.message)
+        src.set(`src: url("${uri}");`)
+      })
+      return src
+    }
+  }
+  function sourcesFromFiles(files) {
+    console.log('files',files)
+    return computed(files.map(sourceFromFile), (...args) => {
+      return args.join('\n')
+    })
+  }
+
+  function cssFromContent(content) {
+    const sources = sourcesFromFiles(content.files)
+    return computed(sources, (...sources) => {
       return `
         @font-face {
           font-family: "${content['font-family']}";
-          ${sources}
+          ${sources.join('\n')}
         }
       `
     })
+  }
+
+  function renderStyleTag(kv, ctx) {
+    const content = kv.value && kv.value.content
+    if (!content) return
 
     return h('style', {
       attributes: {
         "data-key": kv.key
       }
-    }, css) 
+    }, cssFromContent(content)) 
   }
-}
 
-
-function renderPreview(arr) {
-  const font_id = 'font-' + crypto.randomBytes(8).toString('hex')
-
-  const sources = MutantMap(arr, file => {
-    const css = Value('')
-    dataUri(file, (err, uri) => {
-      if (err) return console.error(err.message)
-      css.set(`src: url("${uri}");`)
-    })
-    return css
-  })
-  const css = computed(sources, s => {
-    return `
-      @font-face {
-        font-family: "${font_id}";
-        ${s.join('\n')}
-      }
-    `
-  })
-  return h('div.font-preview', {
-    attributes: {
-      style: `font-family: "${font_id}";`,
-    }
-  }, [
-    h('style', css),
-    [7,9,10,12,16,18,24].map( pt => {
-      return h('div', {
-        attributes: {
-          style: `font-size: ${pt}pt;`
+  function renderPreview(files) {
+    const font_id = 'font-' + crypto.randomBytes(8).toString('hex')
+    const sources = sourcesFromFiles(files)
+    const css = computed(sources, (...sources) => {
+      return `
+        @font-face {
+          font-family: "${font_id}";
+          ${sources.join('\n')}
         }
-      }, `${pt}pt: The quick brown fox jumps over the lazy dog.`)
+      `
     })
-  ])
-}
+    return h('div.font-preview', {
+      attributes: {
+        style: `font-family: "${font_id}";`,
+      }
+    }, [
+      h('style', css),
+      [7,10,16,24].map( pt => {
+        return h('div', {
+          attributes: {
+            style: `font-size: ${pt}pt;`
+          }
+        }, `${pt}pt: The quick brown fox jumps over the lazy dog.`)
+      })
+    ])
+  }
 
-function RenderEditor(ssb, opts) {
-  opts = opts || {}
-  const {prototypes} = opts
-  if (!prototypes) throw new Error('need prototypes!')
-  
-  return function renderEditor(kv, ctx) {
+  function renderEditor(kv, ctx) {
     ctx = ctx || {}
     const content = kv.value && kv.value.content
     if (!content) return
@@ -132,13 +146,8 @@ function RenderEditor(ssb, opts) {
     function removeButton(file) {
       return h('button', {
         'ev-click': e => {
-          const entry = files().find( f => f.name == file.name) 
-          console.log('entry', entry)
-          if (entry) {
-            const f = files()
-            f.delete(entry)
-            set({files: f})
-          }
+          const f = files().filter(f=> f.name !== file.name)
+          set({files: f})
         }
       }, 'remove')
     }
@@ -177,54 +186,47 @@ function RenderEditor(ssb, opts) {
           set({files: f})
         }
       }, [renderList()]),
-      renderPreview(files),
+      computed(files, files => files.length ? renderPreview(files) : []),
       h('button', {
         'ev-click': e => {
           const sourceFiles = files().map(file=>{
-            file.source = opts => FileSource(file, opts)
+            if (!file.link) {
+              file.source = opts => FileSource(file, opts)
+            }
             return file
           })
-          importFonts(ssb, sourceFiles, stati, prototypes, (err, content) => {
+          importFonts(sourceFiles, stati, prototypes, (err, content) => {
             if (err) return console.error(err)
             if (opts.save) opts.save(content)
           })
         }
-      }, 'Apply')
+      }, 'Upload')
     ])
-  }
-}
 
-module.exports = function(ssb, opts) {
-  opts = opts || {}
+    function importFonts(files, stati, prototypes, cb) {
+      const n = files.length
+      if (!n) return cb(null)
+      if (stati().length !== n) {
+        stati.set(Array(n).map(x => Value(false)))
+      }
+      const newFiles = files.filter(f => !f.link)
+      const oldFiles = files.filter(f => f.link)
+      oldFiles.forEach(f => onProgress(f, true))
 
-  const renderEditor = RenderEditor(ssb, opts)
-  const renderCSS = RenderCSS(ssb, opts)
+      importFiles(ssb, newFiles, {prototypes, onProgress}, (err, content) => {
+        if (err) return cb(err)
+        set({files: content.files.concat(oldFiles)})
+      })
 
-  return function render(kv, ctx) {
-    ctx = ctx || {}
-    const content = kv.value && kv.value.content
-    if (content.type !== 'font') return
-
-    function renderTile(kv, ctx) {
-      const content = kv.value && kv.value.content
-      return h('div', {
-        style: { 
-          'font-family': content['font-family'],
-          'font-size': '45px'
-        }
-      }, [
-        renderCSS(kv),
-        'Aa'
-      ])
+      function onProgress(file, err) {
+        let i = files.indexOf(file)
+        console.log('progress', file, i, err)
+        if (err) stati.put(i, err)
+        else stati.put(i, true)
+      }
     }
-
-    if (ctx.where == 'editor') {
-      return renderEditor(kv, ctx)
-    } else if (ctx.where == 'tile') {
-      return renderTile(kv, ctx)
-    }
-    return renderCSS(kv, ctx)
   }
+
 }
 
 // -- utils
@@ -239,18 +241,30 @@ function dataUri(file, cb) {
   reader.readAsDataURL(file)
 }
 
-function importFonts(ssb, files, stati, prototypes, cb) {
-  const n = files.length
-  if (!n) return cb(null)
-  if (stati().length !== n) {
-    stati.set(Array(n).map(x => Value(false)))
-  }
-  importFiles(ssb, files, {prototypes, onProgress}, cb)
 
-  function onProgress(file, err) {
-    let i = files.indexOf(file)
-    console.log('progress', file, i, err)
-    if (err) stat.put(i, err)
-    else stati.put(i, true)
-  }
+function styles() {
+  setStyle(`
+    .tre-fonts-editor .tre-dropzone {
+      min-width: 400px;
+      min-height: 50px;
+      border-radius: 20px;
+      border: 8px dashed #ccc;
+      padding: 1em;
+      margin: 1em;
+    }
+    .tre-fonts-editor .tre-dropzone.drag {
+      border-color: #777;
+    }
+    .tre-fonts-editor .list {
+      display: grid; 
+      grid-template-columns: 4em 3em 1fr 5em 4em; 
+      width: min-content;
+    }
+    .tre-fonts-editor .list > * {
+      margin: .1em .2em;
+    }
+    .tre-fonts-editor .placeholder {
+      color: #555;
+    }
+  `)
 }
